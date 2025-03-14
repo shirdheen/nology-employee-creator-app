e916ac49212c2feaae5bdcc19f2f96eb59afef92
shirdheen <shirdheen2252@gmail.com>
2025-03-14 22:12:51 +1100

    Git initialised

---
+++ src/main/java/com/shirdheen/employee/employee_creator_app_project/EmployeeCreatorAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shirdheen.employee.employee_creator_app_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCreatorAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCreatorAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shirdheen/employee/employee_creator_app_project/EmployeeCreatorAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirdheen.employee.employee_creator_app_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
